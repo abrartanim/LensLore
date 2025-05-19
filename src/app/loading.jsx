4852a1976bba018a6618c0f9c83bf49a6003d809
abrartanim <abrar.tanim@gmail.com>
2025-05-20 02:46:30 +0600

    added movie page

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 export default function loading() {
   return (
-    <img className="flex justify-center" src="spinner.svg" alt="loading..." />
+    <div className="flex justify-center">
+      <img src="spinner.svg" alt="loading..." />
+    </div>
   );
 }
